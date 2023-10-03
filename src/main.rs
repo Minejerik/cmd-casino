b02ae3d25a9440749f42bee19152c79f555fffe5
minejerik <jaanerikf@gmail.com>
2023-10-03 08:45:29 -0400

    working on user input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod bank_utils;
+use std::io;
 
 // pub use crate::bank_utils;
 
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut guess = String::new();
+
+    println!("Would you like to:");
+    println!("(R)egister or (L)ogin");
+
+    io::stdin().read_line(&mut guess).expect("failed to get input");
+
+
     bank_utils::create_new_user("temp".to_string());
 }
