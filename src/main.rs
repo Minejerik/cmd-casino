4777c3a7d2a9e12103b5e1941ba60065e515df26
minejerik <jaanerikf@gmail.com>
2023-10-03 09:20:36 -0400

    working on registering

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 mod bank_utils;
 use std::io;
 
+fn clean_name(name: String) -> String {
+    let temp = name.trim().to_string().replace(" ", "_").replace("\n", "").to_lowercase();
+    return temp;
+}
+
+
+fn register(){
+    let mut username = String::new();
+
+    println!("Please enter a username:");
+    io::stdin().read_line(&mut username).expect("failed to get input");
+
+    username = clean_name(username);
+
+    bank_utils::create_new_user(username);
+
+    println!("Please restart the program to login");
+
+}
+
 fn main() {
 
     let mut guess = String::new();
@@ -11,13 +31,11 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("failed to get input");
 
 
-    println!("{}", guess);
-
     guess = guess.trim().to_string().to_lowercase();
 
 
    match &guess[..] {
-       "r" => println!("temp"),
+       "r" => register(),
        _ => println!("Invalid input"),
    }
 }
